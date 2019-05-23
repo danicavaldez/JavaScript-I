2fe85837285c54acafe0eca089105777d2312acb
Danica Valdez <danicavaldez@Danicas-MacBook-Pro.local>
2019-05-22 20:26:28 -0700

    attempted stretch

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -112,11 +112,10 @@ console.log(oldCars.length);
 let BMWAndAudi = [];
 
 for (let i = 0; i < inventory.length; i++) {
-  if (inventory[i].car_make === 'BMW' || inventory[i].car_make === 'Audi') {
+  if ((inventory[i].car_make === 'BMW') || (inventory[i].car_make === 'Audi')) {
     BMWAndAudi.push(inventory[i]);
   }
 }
 
 console.log(JSON.stringify(BMWAndAudi));
-
 
